9545aa73b7b2af18d867f8b6125dde9733af5311
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-18 18:06:32 +0200

    [add] feat method index

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,12 @@
 const connection = require('../data/db')
 
 const index = (req, res) => {
-  console.log('Lista film')
+  const sql = 'SELECT * FROM movies'
+
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: 'Errore nel caricamento dei film: ' + err });
+    res.send(results)
+  })
 }
 
 const show = (req, res) => {
