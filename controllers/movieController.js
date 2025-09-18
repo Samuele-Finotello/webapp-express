cdd77d64d234a72a1c66c8f999d80dd9cf943ebf
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-18 18:22:52 +0200

    [add] feat method show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -10,7 +10,26 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-  console.log('Film con id ?')
+  const id = req.params.id;
+  const nameSql = 'SELECT * FROM movies WHERE id = ?'
+
+  const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?'
+
+  connection.query(nameSql, [id], (err, resultsName) => {
+    if (err) return res.status(500).json({ error: 'Errore nel caricamento del film: ' + err });
+    if (resultsName.length === 0) return res.status(404).json({ error: 'Film non trovato' })
+
+    connection.query(reviewsSql, [id], (err, resultsReviews) => {
+      if (err) return res.status(500).json({ error: 'Errore nel caricamento del film: ' + err });
+
+      const resultsNameReviews = {
+        ...resultsName[0],
+        reviews: resultsReviews
+      }
+
+      res.send(resultsNameReviews)
+    })
+  })
 }
 
 module.exports = {
