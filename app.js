5d84314234284885cc7db06fc2ac043d0ac9c600
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-21 17:53:09 +0200

    [add] feat import and use method cors

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const cors = require('cors')
+
 const app = express();
 
 const port = process.env.PORT;
 
 const movieRouter = require('./routers/movieRouter')
+
+app.use(cors({ origin: process.env.FE_APP }))
 
 //rotta base
 app.get('/', (req, res) => {
