ef3b4bae376bdb36c9cf78bd02106a88ee820dcf
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-21 18:27:08 +0200

    [add] feat imagePathMiddleware

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const cors = require('cors')
 
+const imagePathMiddleware = require('./middlewares/imagePathMiddleware')
+
 const app = express();
 
 const port = process.env.PORT;
@@ -9,6 +11,8 @@ const port = process.env.PORT;
 const movieRouter = require('./routers/movieRouter')
 
 app.use(cors({ origin: process.env.FE_APP }))
+
+app.use(imagePathMiddleware)
 
 //rotta base
 app.get('/', (req, res) => {
